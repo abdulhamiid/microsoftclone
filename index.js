1bb6b724039c89181579bfef4e3280b10cc91bd3
Abdulhamid <adioabdulhamid@yahoo.com>
2022-01-14 16:54:47 +0100

    add no-scroll effect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let toggler = document.querySelector('.toggler');
 let menu = document.querySelector('.main-menu')
 toggler.addEventListener('click', () => {
-    menu.classList.toggle('show')
+    menu.classList.toggle('show');
+    document.body.classList.toggle('no-scroll')
 })
